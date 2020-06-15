874cc2fd0e0751f2694bae2ebaf5d471dff5f2b7
Maks Kolman <makskolman@google.com>
2020-06-15 10:15:10 +0100

    Start solving project euler in rust

---
+++ src/main.rs
@@ -0,0 +1,8 @@
+mod easy;
+fn main() {
+	println!("001: {}", easy::prob001(1000));
+	println!("002: {}", easy::prob002(4000000));
+	println!("003: {}", easy::prob003(600851475143));
+	println!("004: {}", easy::prob004(100, 999));
+	println!("005: {}", easy::prob005(20));
+}
